3063e85b63c3758ca3c784c70d2a4d579f764e49
learn <dylansalay@yahoo.com>
2019-07-23 11:22:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  Col,
+  Button,
+  Container,
+  Row,
+} from 'react-bootstrap'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      buttonText: "Play Game",
+      gameWon: false
+    }
+  }
+  toggleResult(){
+    const newButtonText = this.state.gameWon ? "Play Game" : "Excellent!"
+    const newGameWon = this.state.gameWon ? false : true
+    this.setState({ buttonText: newButtonText, gameWon: newGameWon})
+
+  }
+  render() {
+    return (
+      <Container>
+        <h1>
+          Testing Example
+        </h1>
+        <Row>
+          <Col xs={4}>
+            <Button onClick={this.toggleResult.bind(this)}>
+              {this.state.buttonText}
+            </Button>
+          </Col>
+          <Col xs={8}>
+            <span id="result-text">
+              {this.state.gameWon &&
+                <h4>Congratulations</h4>
+              }
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
